9fb760cedf225d3833781235fc9400325f0d596e
fatih efkan <mfefkan@hotmail.com>
2023-09-20 13:00:12 +0300

    Changing The Status metodları incelendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,8 +26,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
       </div>
       <button>Send</button>
     </form>
-    <button (click)="ok()">Okey</button> 
-    Valid: {{ frm.valid }}
+
+    <button (click)="markAsTouched()">markAsTouched</button> <br />
+    <button (click)="markAllAsTouched()">markAllAsTouched</button> <br />
+    <button (click)="markAsUnTouched()">markAsUnTouched</button> <br />
+    <button (click)="markAsDirty()">markAsDirty</button> <br />
+    <button (click)="markAsPristine()">markAsPristine</button> <br />
+    <button (click)="disable()">disable</button> <br />
+    <button (click)="enable()">enable</button> <br />
+
+    form touched : {{ frm.touched }} <br />
+    'name' form control touched : {{ frm.get('name').touched }}<br>
+    'address' form control touched : {{ frm.get('address').touched }}<br>
+    <hr>
+    'country' form control touched : {{ frm.get('address').get('country').touched }}<br>
+    <hr />
+    form dirty : {{ frm.dirty }} <br />
+    'name' form control dirty : {{ frm.get('name').dirty }}
+    <hr />
+    form pristine : {{ frm.pristine }} <br />
+    'name' form control pristine : {{ frm.get('name').pristine }}
+
+    <!-- <button (click)="ok()">Okey</button>
+    Valid: {{ frm.valid }} -->
   `,
 })
 export class AppComponent {
@@ -45,20 +66,50 @@ export class AppComponent {
       }),
     });
 
-    //aşağıdaki valueChanges sayesinde konsola girilen her bir veri için anlık olarak değişimi console'a yazdırıyoruz bu şekilde form tetiklendiğinde bir metodu tetikleyebilir. herhangi bir form elemanı için de düzenlenebilir.
-this.frm.valueChanges.subscribe({
-  next:data=>{
-    console.log(data); 
-    
-  }
-})
-
+    // //aşağıdaki valueChanges sayesinde konsola girilen her bir veri için anlık olarak değişimi console'a yazdırıyoruz bu şekilde form tetiklendiğinde bir metodu tetikleyebilir. herhangi bir form elemanı için de düzenlenebilir.
+    // this.frm.valueChanges.subscribe({
+    //   next: (data) => {
+    //     console.log(data);
+    //   },
+    // });
   }
 
-  ok() {
-    this.frm.get('name').setValue('Gençay', { onlySelf: true }); // burada onlySelf true verildiği için validationa girmeden name değiştiğini görebiliriz. okeye basınca validation true'ya dönmüyor hala false kalıyor.
-  }
+  // ok() {
+  //   this.frm.get('name').setValue('Gençay', { onlySelf: true }); // burada onlySelf true verildiği için validationa girmeden name değiştiğini görebiliriz. okeye basınca validation true'ya dönmüyor hala false kalıyor.
+  // }
   onSubmit() {
     console.log(this.frm.value);
+  }
+
+
+  
+ // Change Status Konusu
+  markAsTouched() {
+    //this.frm.markAsTouched();
+    this.frm.get("name").markAsTouched({onlySelf:true});
+  }
+
+  markAllAsTouched() {
+    this.frm.get("address").markAllAsTouched(); //get('address') yaptığımız için adress ve onun altındaki controller etkilendi. get'i kaldırırsak bütün controllerin etkilendiğini görürüz.
+  }
+
+  markAsUnTouched() {
+    this.frm.markAsUntouched();
+  }
+
+  markAsDirty() {
+    this.frm.markAsDirty();
+  }
+
+  markAsPristine() {
+    this.frm.markAsPristine();
+  }
+
+  disable() {
+    this.frm.disable();
+  }
+
+  enable() {
+    this.frm.enable();
   }
 }
